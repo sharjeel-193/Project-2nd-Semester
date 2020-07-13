2677e766718df1a9db240a38f29d086aea040c0a
QasimKhan5x <straightouttadetroit.qk@gamil.com>
2020-07-14 00:01:18 +0500

    add firebase songs

--- app/src/main/java/com/selflearning/starcover/Logic/Instrumental.java
+++ app/src/main/java/com/selflearning/starcover/Logic/Instrumental.java
@@ -4,20 +4,32 @@ public class Instrumental {
 
     private String songName;
     private String artistName;
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
     private int instrumentalImageId;
     private int duration;
 
-    public Instrumental(String songName, String artistName, int instrumentalImageId) {
+    public Instrumental(String songName, String artistName, int instrumentalImageId, String url) {
         this.songName = songName;
         this.artistName = artistName;
         this.instrumentalImageId = instrumentalImageId;
+        this.url = url;
     }
 
-    public Instrumental(String songName, String artistName, int instrumentalImageId, int duration) {
+    public Instrumental(String songName, String artistName, int instrumentalImageId, int duration, String url) {
         this.songName = songName;
         this.artistName = artistName;
         this.instrumentalImageId = instrumentalImageId;
         this.duration = duration;
+        this.url = url;
     }
 
     public String getSongName() {
--- app/src/main/java/com/selflearning/starcover/ui/sing/SingFragment.java
+++ app/src/main/java/com/selflearning/starcover/ui/sing/SingFragment.java
@@ -2,6 +2,7 @@ package com.selflearning.starcover.ui.sing;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,14 +11,23 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 import com.selflearning.starcover.Logic.Instrumental;
 import com.selflearning.starcover.R;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +37,14 @@ public class SingFragment extends Fragment {
     private RecyclerView instrumentalsView;
     List<Instrumental> instrumentalList;
     Instrumental instrumental;
+    private StorageReference mStorageRef;
+
+    List<String> songNames = new ArrayList<>();
+    List<String> songUrls = new ArrayList<>();
+
+    public void onCreate() {
+
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container,
@@ -34,6 +52,37 @@ public class SingFragment extends Fragment {
         singViewModel = ViewModelProviders.of(this).get(SingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sing,container,false);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference("music");
+        mStorageRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for (StorageReference prefix : listResult.getPrefixes()) {
+                            // All the prefixes under listRef.
+                            // You may call listAll() recursively on them.
+                        }
+
+                        for (StorageReference item : listResult.getItems()) {
+                            // All the items under listRef.
+                            Log.d("Qasim", item.getName());
+                            songNames.add(item.getName());
+                            item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    songUrls.add(uri.toString());
+                                }
+                            });
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Uh-oh, an error occurred!
+                    }
+                });
+
+        Log.d("Qasim1", String.valueOf(songNames.size()));
         instrumentalsView =(RecyclerView) root.findViewById(R.id.sing_recycler_view);
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 1);
@@ -42,12 +91,25 @@ public class SingFragment extends Fragment {
         instrumentalsView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
         instrumentalList = new ArrayList<>();
 
-        instrumental = new Instrumental("ab Song","Artist",R.drawable.thumbnail,23);
+
+        for(int i = 0; i < songNames.size(); i++) {
+            if (i % 2 == 0 ) {
+                instrumentalList.add(new Instrumental(songNames.get(i), "Artist",
+                        R.drawable.thumbnail, 23, songUrls.get(i)));
+            } else {
+                instrumentalList.add(new Instrumental(songNames.get(i), "Artist",
+                        R.drawable.thumbnail3, 67, songUrls.get(i)));
+            }
+
+        }
+        Log.d("Qasim", String.valueOf(songNames.size()));
+        Log.d("Qasim", "Hi");
+        instrumental = new Instrumental("ab Song","Artist",R.drawable.thumbnail,23, "www,gooog.eom");
         instrumentalList.add(instrumental);
-        instrumental = new Instrumental("bc song","Artist",R.drawable.thumbnail3,67);
+        instrumental = new Instrumental("bc song","Artist",R.drawable.thumbnail3,67, "www,gooog.eom");
         instrumentalList.add(instrumental);
-        instrumental = new Instrumental("cd Song","Artist",R.drawable.thumbnail,23);
-        instrumentalList.add(instrumental);
+        instrumental = new Instrumental("cd Song","Artist",R.drawable.thumbnail,23, "www,gooog.eom");
+        /*instrumentalList.add(instrumental);
         instrumental = new Instrumental("de song","Artist",R.drawable.thumbnail3,67);
         instrumentalList.add(instrumental);
         instrumental = new Instrumental("df Song","Artist",R.drawable.thumbnail,23);
@@ -65,7 +127,7 @@ public class SingFragment extends Fragment {
         instrumental = new Instrumental("dz Song","Artist",R.drawable.thumbnail,23);
         instrumentalList.add(instrumental);
         instrumental = new Instrumental("gf song","Artist",R.drawable.thumbnail3,67);
-        instrumentalList.add(instrumental);
+        instrumentalList.add(instrumental);*/
 
         final MySingAdapter adapter = new MySingAdapter(getActivity(),instrumentalList);
         instrumentalsView.setAdapter(adapter);
